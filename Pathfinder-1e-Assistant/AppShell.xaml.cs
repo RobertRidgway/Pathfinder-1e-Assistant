a555872fad8dbe4a1afdc9f0e1aa3a9ad351deb5
RobertRidgway <robbieridgway@gmail.com>
2024-04-18 23:52:13 -0600

    Add project files.

---
+++ Pathfinder-1e-Assistant/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pathfinder_1e_Assistant
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pathfinder-1e-Assistant/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Pathfinder_1e_Assistant
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
